798da2b08056aa3dc6a0cde008f37ed40c4c0fbe
alcidesjunior <alcidesjunior.infor@gmail.com>
2016-12-06 15:36:26 -0300

    login em andamento

--- pylocked-api.py
+++ pylocked-api.py
@@ -1,13 +1,18 @@
-from bottle import run, post, get, put, request, response
-import json
+from bottle import run, post, get, put, request, response,HTTPResponse
+import json, database
 
 @post('/login')
 def logar():
 	response.headers['Content-Type']='application/json'
-	
+
 	email = request.forms.get('email')
 	pss  = request.forms.get('password')
+	login = cursor.execute("select count(*) as qtd from users where email='\"%s' and password='\"%s'" % (email,pss))
+	if login == 1:
+		retorno = json.dumps({'user': str(email) , 'password': str(pss)})
+		return bottle.HTTPResponse(status=200, body=retorno)
+	else:
+		return bottle.HTTPResponse(status=400,body="invalid authentication")
 
-	return json.dumps({'user': str(email) , 'password': str(pss)})
 
 run( host='localhost', port=3000)
