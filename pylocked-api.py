a62c9b47fa0f328cb678d697c73fbc51eca22fb7
alcidesjunior <alcidesjunior.infor@gmail.com>
2016-12-06 15:19:27 -0300

    login em andamento

--- pylocked-api.py
+++ pylocked-api.py
@@ -1,26 +1,13 @@
-from bottle import Bottle, run
-import sqlite3
+from bottle import run, post, get, put, request, response
+import json
 
-conn = sqlite3.connect('pylocked.db')
-cursor = conn.cursor()
+@post('/login')
+def logar():
+	response.headers['Content-Type']='application/json'
+	
+	email = request.forms.get('email')
+	pss  = request.forms.get('password')
 
-cursor.execute("""
-create table users (
-	id int primary key auto_increment,
-	email varchar(20),
-	senha varchar(64)
-)
-""")
+	return json.dumps({'user': str(email) , 'password': str(pss)})
 
-
-app = Bottle()
-
-@app.route('/<email>/<senha>')
-def add(email, senha):
-	return "seu email e %s e sua senha e %s" % (email,senha)
-
-@app.route('/hello')
-def hello():
-	return "What up bro?"
-
-run(app, host='localhost', port=3000)
+run( host='localhost', port=3000)
